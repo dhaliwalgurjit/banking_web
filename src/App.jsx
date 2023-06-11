94607740464c72fda8e567d419c4139d5bd9cd6d
Gurjit Dhaliwal <Gurjit Dhaliwal@LAPTOP-MALATNV4>
2023-06-11 14:51:19 +0530

    app_completed

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import styles from './style';
+
+import {
+  Navbar, Hero, Stats, Business, Billing, CardDeal, Testimonials,
+  Clients, CTA, Footer
+} from './components';
+
+const App = () => (
+  <div className="bg-black w-full overflow-hidden">
+    {/* <div className="container mx-auto"> */}
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Navbar />
+      </div>
+    </div>
+
+    <div className={`bg-black ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Hero />
+      </div>
+    </div>
+    <div className={`bg-black ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Stats />
+        <Business />
+        <Billing />
+        <CardDeal />
+        <Testimonials />
+        <Clients />
+        <CTA />
+        <Footer />
+      </div>
+    </div>
+    {/* </div> */}
+  </div>
+)
+
+
+export default App
